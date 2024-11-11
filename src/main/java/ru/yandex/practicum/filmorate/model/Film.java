9ab68821ccd5d65fac89b258e314dfbbef594817
Екатерина <pygavko@yandex.ru>
2024-11-11 16:18:14 +0300

    Создать каркас Spring Boot приложения Filmorate.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+
+    Long id;
+
+    @NotNull
+    @NotBlank
+    String name;
+
+    @Size(max = 200)
+    String description;
+
+    LocalDate releaseDate;
+
+    @Positive
+    Long duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    Long id;
+
+    @NotNull
+    @Email
+    String email;
+
+    @NotNull
+    @NotBlank
+    String login;
+
+    String name;
+    LocalDate birthday;
+}
